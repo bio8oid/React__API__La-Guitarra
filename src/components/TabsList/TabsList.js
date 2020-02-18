094c3a9f9efb4812d30e8a71cb20ba6fe91a9bff
bio8oid <8bollod8@mail.co.uk>
2020-02-18 22:19:36 +0100

    Refactoring state to hooks

--- src/components/TabsList/TabsList.js
+++ src/components/TabsList/TabsList.js
@@ -1,61 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './TabsList.scss';
 
-class TabsList extends React.Component {
+const TabsList = props => {
 
-    state = {
-        page: 1,
-        tabsList: []
+    const [page, setPage] = useState(1);
+
+    const handlePage = id => e => {
+        setPage(id);
     }
 
-    handlePage = () => e => {
-        let id = e.target.id;
-        this.setState({ page: id });
-    }
+    useEffect(() => setPage(1), [props.tabsList]);
 
-    static getDerivedStateFromProps(props, tabsList) {
-        if (props.tabsList !== tabsList) {
-            return { tabsList: props.tabsList };
-        }
-        else return tabsList;
-    }
+    const dataset = props.tabsList;
+    const pageSet = page;
+    const offset = (pageSet - 1) * 10
+    const paginatedItems = dataset.slice(offset).slice(0, 10)
+    const pages = Math.ceil(dataset.length / 10);
+    const pageNumbers = Array.from({ length: pages }, (x, page) => ++page);
 
-    render() {
+    return (
 
-        console.log(this.props.tabsList)
+        <div className="list-wrapper">
 
-        const dataset = this.props.tabsList;
-        const pageSet = this.state.page;
-        const offset = (pageSet - 1) * 10
-        const paginatedItems = dataset.slice(offset).slice(0, 10)
-        const pages = Math.ceil(dataset.length / 10);
-        const pageNumbers = Array.from({ length: pages }, (x, page) => ++page);
+            {paginatedItems.map(item => (
 
-        return (
-
-            <div className="list-wrapper">
-
-                {paginatedItems.map(item => (
-
-                    <div className="list-item" key={item.id}>
-                        <h2>{item.artist.name}</h2>
-                        <p>{item.title}</p>
-                        {/* <div className="image-and-tags-wrapper"> */}
-                            <div className="list-type-tags">
-                                {item.tabTypes.map((tagItem, index) => (<a key={index} className="tag" href={`http://www.songsterr.com/a/wa/song?id=${item.id}`} target="blank">{tagItem}</a>))}
-                            </div>
-                        {/* </div> */}
-                    </div>))}
-                <div className="pagination-buttons">
-                    {pageNumbers.map(id => {
-                        return (
-                            <button className="pagination-button" id={id} key={id} onClick={this.handlePage(id)}>{id}</button>
-                        );
-                    })}
-                </div>
+                <div className="list-item" key={item.id}>
+                    <h2>{item.artist.name}</h2>
+                    <p>{item.title}</p>
+                    <div className="list-type-tags">
+                        {item.tabTypes.map((tagItem, index) => (<a key={index} className="tag" href={`http://www.songsterr.com/a/wa/song?id=${item.id}`} target="blank">{tagItem}</a>))}
+                    </div>
+                </div>))}
+            <div className="pagination-buttons">
+                {pageNumbers.map(id => {
+                    return (
+                        <button className="pagination-button" id={id} key={id} onClick={handlePage(id)}>{id}</button>
+                    );
+                })}
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default TabsList;
--- src/container/MainLayout.js
+++ src/container/MainLayout.js
@@ -1,82 +1,66 @@
-import React,{Component} from "react";
+import React, { useState } from 'react';
 import SearchComponent from "../components/SearchComponent/SearchComponent";
 import TabsList from "../components/TabsList/TabsList";
 import './MainLayout.scss';
 
-class MainLayout extends Component {
+const MainLayout = () => {
 
-    state = {
-        tabsList: [],
-        inputData: [],
-        dropdownData: "PLAYER"
-    }
+    const [tabsList, setTabsList] = useState([]);
+    const [inputData, setInputData] = useState({});
+    const [dropdownData, setDropdownData] = useState("PLAYER");
 
-    getList = async () => {
+    const getList = async () => {
         const prefix = "https://cors-anywhere.herokuapp.com/";
         const url = "https://www.songsterr.com/a/ra/songs.json?pattern=";
 
-        console.log(this.state.inputData)
-        console.log(this.state.dropdownData)
+        console.log(inputData)
+        console.log(dropdownData)
 
         try {
-            const res = await fetch(prefix +  url + this.state.inputData);
+            const res = await fetch(prefix + url + inputData);
             const data = await res.json();
-            this.createTabsList(data);
+            createTabsList(data);
         } catch (err) {
             console.log(err);
         }
     }
     
-    createTabsList = data => {
-        if (this.state.dropdownData !== "PLAYER"){
-        const filteredData = data.filter(x => x.tabTypes.find(x => x === this.state.dropdownData))
-        console.log(filteredData);
-        this.setState({ tabsList: filteredData });
+    const createTabsList = data => {
+        if (dropdownData !== "PLAYER"){
+        const filteredData = data.filter(x => x.tabTypes.find(x => x === dropdownData))
+            setTabsList(filteredData);
         }else{
-            this.setState({ tabsList: data });
+            setTabsList(data);
         }
     }
 
-    buttonHandle = e => {
+    const buttonHandle = e => {
         e.preventDefault();
-        this.getList();
+        getList();
     }
 
-    handleChange = e => {
-        // console.log(e.target)
-        // console.log(e.target.value)
-        // console.log(e.target.type)
-        // console.log(e.target.name)
-        // const value = e.target.value;
-        // const name = e.target.name;
+    const handleChange = e => {
 
         const { name, value } = e.target;
-        this.setState({ [name]: value })
 
-        // return name === 'inputData' ? this.setState({ inputData: value }) :
-        // name === 'dropdownData' ? this.setState({ dropdownData: value }) : value;
-
-        // if (name === 'inputData') {
-        //     this.setState({ inputData: value });
-        // }
-        // if (name === 'dropdownData') {
-        //     this.setState({ dropdownData: value });
-        // }
-
+        if (name === 'inputData') {
+            setInputData(value);
+        }
+        if (name === 'dropdownData') {
+            setDropdownData(value);
+        }
     }
 
-    render() {
+    // render() {
         return (
             <div className="main-component">
-                <SearchComponent onChange={e => this.handleChange(e)} 
-                onSubmit={e => { this.buttonHandle(e) }} />
-                    {/* <div className="background-list"> */}
-                <TabsList tabsList={this.state.tabsList} />
+                <SearchComponent onChange={e => handleChange(e)} 
+                onSubmit={e => { buttonHandle(e) }} />
+                <TabsList tabsList={tabsList} />
                 <div className="logo"><h3>© bio8oid</h3></div>
-                {/* </div> */}
             </div>
         );
     }
-}
+// }
 
 export default MainLayout;
